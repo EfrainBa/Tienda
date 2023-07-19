fd87e19afd5c4f939a5a665cecdf51782f89b475
Efrain Batista <efrainbatista13@outlook.com>
2023-07-19 12:06:11 -0500

    Initial commit

---
+++ src/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Teclado Mecanico RGB',
+        description: 'Teclado Mecanico con luces RGB switches cherry red',
+        price: 32.50
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV 55',
+        description: 'Excelente tv LCD...',
+        price: 450.75
+    },
+    {
+        id: 3,
+        name: 'Audifono Bluetooth Sony',
+        description: 'Audifono Bluetooth para smartphone',
+        price: 45.30
+    },
+    {
+        id: 4,
+        name: 'Memoria Corsair 8GB DDR5',
+        description: 'Memoria RAM optimizada para Juegos',
+        price: 75.10
+    },
+    {
+        id: 5,
+        name: 'Asus Nvidia RTX',
+        description: 'Tarjeta Grafica para juegos en 4k',
+        price: 240.30
+    },
+    {
+        id: 6,
+        name: 'CPU Intel Core i5',
+        description: 'Optimizada para tareas multicore',
+        price: 205.30
+    },
+]
